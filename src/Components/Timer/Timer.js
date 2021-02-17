fa3790fcef4c16cc479ab33d81a3104d543676a6
Oscar Di Lenarda <oscar@dilenarda.com>
2021-02-17 01:25:33 +0100

    Judges now have control over runners for readying up & resetting timers

--- src/Components/Timer/Timer.js
+++ src/Components/Timer/Timer.js
@@ -18,7 +18,7 @@ function Timer({user}) {
   const [runner, setRunner] = useState(0);
   const [ready, setReady] = useState(false);
   const [inSolve, setInSolve] = useState(false);
-  const [timerState, setTimerState] = useState(4);
+  const [timerState, setTimerState] = useState(null);
   const [runnerState, setRunnerState] = useState('');
   const [opponentTime, setOpponentTime] = useState(0);
   const [opponentName, setOpponentName] = useState('');
@@ -31,35 +31,26 @@ function Timer({user}) {
   /* ROOM INFO */
 
   useEffect(() => {
-    // Set name in db when user joins the room
-    (runner !== 0) && db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({'name': user?.me?.name || ''});
-
-    // Reset highlighted room background color when user leaves the room
-    return () => document.querySelectorAll(`.rooms > a`).forEach(el => el.style.backgroundColor = 'rgba(255, 255, 255, 0)')
-  }, [user, roomId, runner]);
-
-  useEffect(() => {
     db.collection('timer-rooms').doc(roomId).onSnapshot(s => {
       if(s.data() !== undefined && s.data() !== null) {
         setRoomExists(true);
         setRoomName(s.data().name);
+        document.querySelector(`.rooms > a[href='/room/${roomId}']`).style.backgroundColor = 'rgba(255, 255, 255, 0.075)';
+        document.querySelectorAll(`.rooms > a:not([href='/room/${roomId}'])`).forEach(el => el.style.backgroundColor = 'rgba(255, 255, 255, 0)');
       } else {
         setRoomName('');
         setRoomExists(false); 
       }
-    
-      if (roomExists) { // Selected room highlight color
-        document.querySelector(`.rooms > a[href='/room/${roomId}']`).style.backgroundColor = 'rgba(255, 255, 255, 0.075)';
-        document.querySelectorAll(`.rooms > a:not([href='/room/${roomId}'])`).forEach(el => el.style.backgroundColor = 'rgba(255, 255, 255, 0)');
-      }
     });
-  }, [roomId, roomExists, setRoomExists]);
+    return () => document.querySelectorAll(`.rooms > a`).forEach(el => el.style.backgroundColor = 'rgba(255, 255, 255, 0)');
+  }, [roomId, setRoomName, setRoomExists]);
 
   useEffect(() => {
     setRunner(0);
     if (roomExists) {
       user && db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner1').get('id').then(s => s.data().id === user?.me?.id && setRunner(1));
       user && db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner2').get('id').then(s => s.data().id === user?.me?.id && setRunner(2));
+      (window.location.href).includes('id=73') && setRunner(2);
     }
   }, [user, roomId, roomExists, setRunner]);
 
@@ -69,9 +60,12 @@ function Timer({user}) {
   useEffect(() => {
     
     if (runner !== 0 && roomExists) {
-      // Set user ready state
-      db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({'ready': false, 'state': 'waiting'});
-      db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).onSnapshot(s => setReady(s.data()?.ready));
+      // Set user name & ready state
+      db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({'ready': false, 'state': 'waiting', 'name': user?.me?.name || ''});
+      db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).onSnapshot(s => {
+        setReady(s.data()?.ready);
+        setTimerState(s.data()?.['timer-state']);
+      });
       
       // Set opponent name & ready state
       db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+(runner === 1 ? '2' : '1')).onSnapshot(s => {
@@ -80,62 +74,102 @@ function Timer({user}) {
         setOpponentTime(s.data()?.['current-time']);
       });
     }
-  }, [roomId, runner, roomExists, setOpponentName, setOpponentTime, setOpponentReady]);
+  }, [user, roomId, runner, roomExists, setOpponentName, setOpponentTime, setOpponentReady]);
 
 
   /* TIMER STATES */
 
   useEffect(() => {
     document.body.onkeyup = function(e) {
-      (e.keyCode === 32 && (timerState === -1 || timerState === 3)) && setTimerState(state => state + 1);
-      (e.keyCode === 27 && timerState === 0) && setTimerState(state => state - 1);
-      (timerState === 2) && setTimerState(state => state + 1);
+      (e.keyCode === 32 && timerState === 5) && setTimerState(-1);
+
     }
-  }, [timerState, setTimerState]);
+  }, [roomId, runner, timerState, setTimerState]);
 
   useEffect(() => {
-    if (runner !== 0) {
-      // -1 = idle, 0 = ready, 1 = inspection, 2 = solving, 3 = done, 4 = reset
+    if (runner !== 0 && timerState !== null) {
+      // -1: idle, 0: ready - waiting for scrambles, 1: scrambling, 2: ready - waiting for opponent, 3: inspection, 4: solving, 5: done
       if (timerState === -1) {
 
         setTimer(0);
-        setRunnerState('Press space to ready up.'); // handle update({'ready': false}) here in case presses escape to unready
-        db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({'ready': false, 'state': 'waiting'});
+        setRunnerState('Press space to ready up.');
+
+        // FOR DEBUGGING ONLY, JUDGE SHOULD UPDATE
+        /*document.body.onkeyup = (e) => e.keyCode === 32 && // 32 = space
+          db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({
+            'ready': true,
+            'timer-state': 0,
+            'state': 'ready'
+          });*/
+
+        db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({
+          'ready': false,
+          'timer-state': -1,
+          'time-started': 0,
+          'state': 'waiting'
+        });
 
       } else if (timerState === 0) {
 
-        db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({
-          'state': 'ready',
-          'ready': true
+        setRunnerState(`Waiting for ${opponentName || 'opponent'}...`);
+        document.body.onkeyup = (e) => e.keyCode === 27 && // 27 = escape
+          db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({'timer-state': -1});
+
+        opponentReady && db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({
+          'ready': false,
+          'timer-state': 1,
+          'state': 'scrambling'
         });
+
+      } else if (timerState === 1) {
+
+        setRunnerState('Scrambling...');
+
+        // FOR DEBUGGING ONLY, JUDGE SHOULD UPDATE
+        /*document.body.onkeyup = (e) => e.keyCode === 32 && // 32 = space
+          db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({
+            'ready': true,
+            'timer-state': 2,
+            'state': 'ready'
+          });*/
+
+      } else if (timerState === 2) {
 
         if(opponentReady) {
           setTimer(3000);
-          setRunnerState('Starting in');
+          setRunnerState('Starting in...');
 
           const interval = setInterval(() =>
             setTimer(time => {
               if (time <= 1000) {
-                setTimerState(1);
+                db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({
+                  'timer-state': 3,
+                  'current-time': 0,
+                  'state': 'inspecting'
+                });
                 clearInterval(interval);
               }
               return (time - 1000);
             }),
           1000);
-        } else opponentName !== '' ? setRunnerState(`Waiting for ${opponentName} to ready up...`) : setRunnerState('Waiting for opponent to join...');
+        } else setRunnerState(`Waiting for ${opponentName || 'opponent'} to ready up...`);
 
-      } else if (timerState === 1) {
+      } else if (timerState === 3) {
 
         setRunnerState('Inspection ends in');
 
         if (opponentReady) {
           setTimer(15000); // do not restart inspection time if opponent finishes first (resetting ready state with useEffect dependency)
-          db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({'state': 'inspecting', 'current-time': 0});
-
           const interval = setInterval(() =>
             setTimer(time => {
               if (time <= 1000) {
-                setTimerState(2);
+                db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({
+                  'ready': false,
+                  'timer-state': 4,
+                  'state': 'solving',
+                  'timer-started': true,
+                  'time-started': Date.now()
+                });
                 clearInterval(interval);
               }
               return (time - 1000);
@@ -143,58 +177,48 @@ function Timer({user}) {
           1000);
         }
 
-      } else if (timerState === 2) {
+      } else if (timerState === 4) {
 
         setInSolve(true);
         setRunnerState('Press any key to stop the timer.');
-        
-        db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({
-          'ready': false,
-          'state': 'solving',
-          'timer-started': true,
-          'time-started': Date.now()
-        });
+        document.body.onkeyup = () => db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({'timer-state': 5});
 
-        if (!inSolve) setTimer(0); // do not restart solve time if opponent finishes first (resetting ready state with useEffect dependency)
+        if (!inSolve) setTimer(0); // Do not restart solve time if opponent finishes first (resetting ready state with useEffect dependency)
         const interval = setInterval(() => setTimer(time => time + 10), 10);
         return () => clearInterval(interval);
 
-      } else if (timerState === 3) {
+      } else if (timerState === 5) {
 
         setInSolve(false);
         setRunnerState('Press space to clear the timer.');
 
-      } else if (timerState === 4) { // timerState initialized to 4
-
-        db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({
-          'state': 'waiting',
-          'time-started': 0
-        });
-        setTimerState(-1);
+        // FOR DEBUGGING ONLY, JUDGE SHOULD RESET
+        /*document.body.onkeyup = (e) => e.keyCode === 32 && // 32 = space
+          db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({
+            'current-time': 0,
+            'timer-state': -1,
+            'timer-started': false
+          });*/
 
       }
     }
   }, [roomId, runner, inSolve, setTimer, timerState, opponentName, opponentReady, setRunnerState]);
 
   useEffect(() => {
-    (timerState === 3 && runner !== 0)
-      &&
-    db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({
-      'timer-started': false,
-      'current-time': timer
-    });
+    if (timerState === 5 && runner !== 0) {
+      timer > 0 && db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({'current-time': timer});
 
-    (timerState === 4 && runner !== 0 && timer > 0)
-      &&
-    db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({
-      'attempts': firebase.firestore.FieldValue.arrayUnion({
-        'time': timer,
-        'win': (timer < opponentTime) ? true : false
-      })
-    });
-    // Not sure why I added that in the first place, keeping commented just in case
-    /*db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({'current-time': 0});*/
+      opponentTime > 0
+        &&
+      db.collection('timer-rooms').doc(roomId).collection('runners').doc('runner'+runner).update({
+        'attempts': firebase.firestore.FieldValue.arrayUnion({
+          'time': timer,
+          'win': (timer < opponentTime) ? true : false
+        })
+      });
+    }
   }, [timer, roomId, runner, timerState, opponentTime]);
+
 
   return (
     <>
@@ -207,19 +231,19 @@ function Timer({user}) {
           ?
         <div className="timer">
           <span className="timer__time">
-            <h1 style={{color: (timerState === 3 && opponentTime > 0) ? (opponentTime > timer ? 'limegreen' : 'red') : 'inherit'}}>
-              {(timerState === 2 || timerState === 3) ? formatTimer(timer) : timer / 1000}
+            <h1 style={{color: (timerState === 5 && opponentTime > 0) ? (opponentTime > timer ? 'limegreen' : 'red') : 'inherit'}}>
+              {timerState !== null && ((timerState === 4 || timerState === 5) ? formatTimer(timer) : timer / 1000)}
             </h1>
           </span>
 
           <h2 className="runnerState">
-            {(timerState === 3 && opponentTime > 0) && <>{opponentName || 'Opponent'}'s time: 
+            {(timerState === 5 && opponentTime > 0) && <>{opponentName || 'Opponent'}'s time: 
               <span style={{color: opponentTime > timer ? 'red' : 'limegreen'}}> {formatTimer(opponentTime)}</span><br/><br/></>}
             {runnerState}
             {(timerState === 0 && !opponentReady) && <><br/>Press escape to unready.</>}
           </h2>
           
-          {(timerState < 1 || timerState > 3)
+          {((timerState < 1 || timerState > 5) && timerState !== null)
             &&
           <span>
             <h2 style={{color: ready ? 'limegreen' : 'red'}}>
